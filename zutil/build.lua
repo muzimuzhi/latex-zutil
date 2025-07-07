63c33508a4b9929d237b60380d026cfe671a176c
Yukai Chou <muzimuzhi@gmail.com>
2025-07-08 03:50:49 +0800

    feat(zutil): add `zutil-regression-test.tex`

--- zutil/build.lua
+++ zutil/build.lua
@@ -3,8 +3,8 @@ module = "zutil"
 
 maindir         = ".."
 
-sourcefiles     = { "*.sty", "*.code.tex" }
-installfiles    = { "*.sty", "*.code.tex" }
+sourcefiles     = { "*.sty", "*.tex" }
+installfiles    = { "*.sty", "*.tex" }
 
 checkengines    = { "pdftex", "xetex", "luatex" }
 stdengine       = "pdftex"
