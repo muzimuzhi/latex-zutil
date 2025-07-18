97de17a058069db27ef34fa1cdca9c63e00adb7b
Yukai Chou <muzimuzhi@gmail.com>
2025-07-19 03:12:34 +0800

    feat(zutil): restore long message in exp errors
    Also run tests with longer `errorline` and `halferrorline`.

--- zutil/build.lua
+++ zutil/build.lua
@@ -9,3 +9,11 @@ installfiles    = { "*.sty", "*.tex" }
 checkengines    = { "pdftex", "xetex", "luatex" }
 stdengine       = "pdftex"
 checksuppfiles  = { "zutil-regression-test.cfg" }
+
+-- show message of expandable errors as full as possible
+-- these are the maximum values accepted by the TeX Live, see texmf.cnf
+--
+-- setting them in the top-level `build.lua` file doesn't work, because
+-- running `l3build` in `./zutil` won't even load the top-level `build.lua`.
+errorline       = 254
+halferrorline   = 239
