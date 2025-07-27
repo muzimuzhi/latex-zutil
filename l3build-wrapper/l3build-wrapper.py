a6d53e4cb99a2d9f37193b71667d1ea67a597b27
Yukai Chou <muzimuzhi@gmail.com>
2025-07-27 12:03:44 +0800

    refactor(wrapper): logging and more (#35)
    * refactor(wrapper): logging level
    
    * refactor(wrapper): `debug_logging_enabled()`
    
    * store `target` as class variable of `TestSuiteRun`
    
    * logging
    
    * move `parse_known_names()` to a class method
    
    * warn on no l3build invocation
    
    * help message
    
    * logging format
    
    * passing `-v` on to l3build needs debug logging
    
    * chore(wrapper): drop a duplicate attribute from logging format
    
    * set most options once for all test suites
    
    * more logging
    
    * ruff format
    
    * add right-padding to logging format attributes
    
    * attach logging to wrap_l3build()
    
    * ruff format
    
    * avoid same named class & instance variable `options`
    
    * change single-usage `add_name()` to nested function

--- l3build-wrapper/l3build-wrapper.py
+++ l3build-wrapper/l3build-wrapper.py
@@ -22,9 +22,6 @@ from typing import Final
 type Test = str
 
 
-logging.basicConfig(
-    format='[%(name)s] %(levelname)s: %(message)s',
-)
 logger = logging.getLogger('wrapper')
 
 
@@ -99,6 +96,9 @@ class TestSuite:
 class TestSuiteRun:
     """Data needed by running l3build on a single test suite."""
 
+    target: Target
+    options_shared: list[str]
+
     def __init__(self, ts: TestSuite) -> None:
         self.name = ts.name
         self.ts = ts
@@ -106,65 +106,107 @@ class TestSuiteRun:
         self.names: list[Test] = []
         self.run_as_whole: bool = False
 
-    def add_name(self, name: Test) -> None:
-        """Add a test name to the test suite run."""
-        if name not in self.names:
-            self.names.append(name)
+    @classmethod
+    def set_shared_target(cls, target: Target) -> None:
+        """Set the target for all test suite runs."""
+        cls.target = target
 
-    def _add_option(self, option: str) -> None:
-        self.options.append(option)
+    @classmethod
+    def set_shared_options(cls, args: argparse.Namespace) -> None:
+        """Set options for all test suite runs."""
 
-    def finalize_names(self, args: argparse.Namespace) -> None:
+        def add_option(option: str) -> None:
+            _options.append(option)
+
+        _options = []
+        if args.stdengine:
+            add_option('-s')
+        if args.quiet:
+            add_option('-q')
+        if logger.getEffectiveLevel() == logging.DEBUG and l3build_patched():
+            add_option('-v')
+        if args.halt_on_error:
+            add_option('-H')
+        if cls.target == Target.CHECK and args.show_saves:
+            add_option('-S')
+        if args.dev:
+            add_option('--dev')
+        if args.dirty:
+            add_option('--dirty')
+        if args.show_log_on_error:
+            add_option('--show-log-on-error')
+        cls.options_shared = _options
+
+    def finalize_names(self) -> None:
         """Adjust collected test names at final stage."""
         if self.run_as_whole:
             # `save` a testsuite means saving all names in it
-            if args.target == Target.SAVE:
+            if self.target == Target.SAVE:
+                logger.info(
+                    'Saving all tests in test suite "%s"',
+                    self.ts.name,
+                )
                 self.names = list(self.ts.get_names())
             # `check` a testsuite means checking with no names
-            elif args.target == Target.CHECK:
+            elif self.target == Target.CHECK:
                 self.names = []
 
-    def set_options(self, args: argparse.Namespace) -> None:  # noqa: C901
+    def set_options(self, args: argparse.Namespace) -> None:
         """Compose l3build options."""
-        if self.ts.config:
-            self._add_option(f'-c{self.ts.config}')
-        if args.engine:
-            self._add_option(f'-e{args.engine}')
-        if args.stdengine:
-            self._add_option('-s')
-        if args.quiet:
-            self._add_option('-q')
-        if args.verbose and l3build_patched():
-            self._add_option('-v')
-        if args.halt_on_error:
-            self._add_option('-H')
-        if args.target == Target.CHECK and args.show_saves:
-            self._add_option('-S')
-        if args.dev:
-            self._add_option('--dev')
-        if args.dirty:
-            self._add_option('--dirty')
-        if args.show_log_on_error:
-            self._add_option('--show-log-on-error')
 
-    def run_l3build(self, target: Target) -> bool:
+        def add_option(option: str) -> None:
+            self.options.append(option)
+
+        if self.ts.config:
+            add_option(f'-c{self.ts.config}')
+        if args.engine:
+            add_option(f'-e{args.engine}')
+
+    def parse_known_names(
+        self,
+        names: set[str],
+    ) -> list[str]:
+        """Parse names received from the command line."""
+
+        def add_name(name: Test) -> None:
+            """Add a test name."""
+            if name not in self.names:
+                self.names.append(name)
+
+        ts = self.ts
+        names_unknown = []
+        for name in names:
+            if name in (ts.name, ts.alias):
+                self.run_as_whole = True
+                logger.debug(
+                    'Name "%s" recognized as a test suite "%s"',
+                    name, ts.name,
+                )  # fmt: skip
+            elif name in ts.get_names():
+                add_name(name)
+                logger.debug(
+                    'Name "%s" recognized as a test in test suite "%s"',
+                    name, ts.name,
+                )  # fmt: skip
+            else:
+                names_unknown.append(name)
+        return names_unknown
+
+    def invoke_l3build(self, args: argparse.Namespace) -> bool:
         """Run l3build on this test suite."""
         if not self.run_as_whole and not self.names:
             return False
 
-        self.finalize_names(args)
+        self.finalize_names()
         self.set_options(args)
+        self.options.extend(TestSuiteRun.options_shared)
 
-        commands = ['l3build', target, *self.options, *self.names]
-        if args.dry_run or args.verbose:
-            logger.info(
-                'Running "%s" in directory "%s"',
-                ' '.join(commands),
-                self.ts.path,
-            )
+        commands = ['l3build', self.target, *self.options, *self.names]
+        path = self.ts.path
+        logger.info('Running "%s" in directory "%s"', ' '.join(commands), path)
         if not args.dry_run:
             try:
-                run(commands, cwd=self.ts.path, check=True)  # noqa: S603
+                run(commands, cwd=path, check=True)  # noqa: S603
             except CalledProcessError:
                 sys.exit(1)
         return True
@@ -192,6 +234,9 @@ tblr_old = TestSuite(
     config='config-old',
     tests=['testfiles-old/*.tex'],
 )
+
+LOGGING_DEFAULT_FORMAT = '[%(name)s] %(levelname)s: %(message)s'
+LOGGING_DEBUG_FORMAT = '[%(name)s] %(levelname)-5s - %(filename)s:%(lineno)d - %(funcName)-17s - %(message)s'  # noqa: E501
 
 L3BUILD_TESTSUITES: Final[tuple[TestSuite, ...]] = (zutil, tblr, tblr_old)
 L3BUILD_TESTSUITES_MAP: Final[dict[str, TestSuite]] = {
@@ -222,71 +267,52 @@ def on_ci() -> bool:
     return os.getenv('CI') == 'true'
 
 
-def debug_logging_enabled() -> bool:
-    """Check if debug logging is enabled."""
-    # debug logging envvars
+def github_debug_logging_enabled() -> bool:
+    """Check if GitHub Actions debug logging is enabled."""
     # https://docs.github.com/en/actions/how-tos/monitoring-and-troubleshooting-workflows/troubleshooting-workflows/enabling-debug-logging
-    return 'DEBUG' in os.environ or (
-        on_ci()
-        and (
-            os.getenv('ACTIONS_RUNNER_DEBUG') == 'true'
-            or os.getenv('ACTIONS_STEP_DEBUG') == 'true'
-        )
+    return (
+        os.getenv('ACTIONS_RUNNER_DEBUG') == 'true'
+        or os.getenv('ACTIONS_STEP_DEBUG') == 'true'
     )
 
 
-def init_logging() -> None:
-    """Initialize logging configuration."""
-    if debug_logging_enabled():
-        logging.basicConfig(
-            format='%(levelname)s - %(filename)s:%(lineno)d - %(funcName)s - %(levelname)s - %(message)s',  # noqa: E501
-        )
-        logger.setLevel(logging.DEBUG)
-        logger.debug('Debug logging is enabled')
-    else:
-        logger.setLevel(logging.WARNING)
+def debug_logging_enabled() -> bool:
+    """Check if debug logging is enabled by environment variables."""
+    return 'DEBUG' in os.environ or (on_ci() and github_debug_logging_enabled())
 
 
-def update_logging(verbosity: int) -> None:
-    """Update logging level based on verbosity."""
-    level = VERBOSITY_TO_LEVEL.get(verbosity, logging.DEBUG)
-    if level < logger.getEffectiveLevel():
+def set_logging_level(args: argparse.Namespace) -> None:
+    """Set logging level."""
+
+    def set_level(level: int) -> None:
+        if level == logging.DEBUG:
+            logging.basicConfig(format=LOGGING_DEBUG_FORMAT)
+        else:
+            logging.basicConfig(format=LOGGING_DEFAULT_FORMAT)
         logger.setLevel(level)
-        logger.debug('Logging level set to %s', logging.getLevelName(logger.level))
+        logger.debug('Logging level set to %s', logging.getLevelName(level))
 
+    level = VERBOSITY_TO_LEVEL.get(args.verbose, logging.DEBUG)
+    if debug_logging_enabled():
+        level = min(level, logging.DEBUG)
+    elif args.dry_run:
+        level = min(level, logging.INFO)
 
-def parse_known_names(
-    names: list[str],
-    testsuites_run: dict[str, TestSuiteRun],
-) -> None:
-    """Parse names received from the command line."""
-    _names = set(names)
-    for name in _names.copy():
-        for ts in L3BUILD_TESTSUITES:
-            ts_run = testsuites_run[ts.name]
-            if name in (ts.name, ts.alias):
-                _names.remove(name)
-                ts_run.run_as_whole = True
-                logger.debug(
-                    'Name "%s" recognized as a test suite "%s"',
-                    name, ts.name,
-                )  # fmt: skip
-            elif name in ts.get_names():
-                _names.remove(name)
-                ts_run.add_name(name)
-                logger.debug(
-                    'Name "%s" recognized as a test in test suite "%s"',
-                    name, ts.name,
-                )  # fmt: skip
-    if _names:
-        raise UnknownNameError(_names.pop())
+    set_level(level)
 
 
 def wrap_l3build(args: argparse.Namespace) -> None:
     """Run l3build on one test suite a time."""
+    logger.debug('Parsed args: %s', args)
+
     target = args.target
     if target not in Target:
         raise UnknownTargetError(target)
+
+    TestSuiteRun.set_shared_target(Target(target))
+    logger.debug('Shared target: "%s"', target)
+    TestSuiteRun.set_shared_options(args)
+    logger.debug('Shared options: %s', TestSuiteRun.options_shared)
 
     testsuites_run: dict[str, TestSuiteRun] = {
         ts.name: TestSuiteRun(ts) for ts in L3BUILD_TESTSUITES
@@ -299,11 +325,21 @@ def wrap_l3build(args: argparse.Namespace) -> None:
         for ts_run in testsuites_run.values():
             ts_run.run_as_whole = True
     else:
-        parse_known_names(args.names, testsuites_run)
+        # parse names
+        names = set(args.names)
+        unknown = names.copy()
+        for ts_run in testsuites_run.values():
+            unknown = unknown.intersection(ts_run.parse_known_names(names))
+        if unknown:
+            raise UnknownNameError(unknown.pop())
 
     # run l3build
+    invoked = False
     for ts_run in testsuites_run.values():
-        ts_run.run_l3build(target)
+        invoked |= ts_run.invoke_l3build(args)
+    if not invoked:
+        # should not happen, but just in case
+        logger.warning('No l3build commands were invoked.')
 
 
 # Unlike in vanilla l3build, options can be intermixed with names,
@@ -325,7 +361,7 @@ parser.add_argument('names', type=str, nargs='*', metavar='name',
 parser.add_argument('-n', '--dry-run', action='store_true', default=False,
                     help='print what l3build command(s) would be executed without execution')  # noqa: E501
 parser.add_argument('-v', '--verbose', action='count', default=0,
-                    help='print more information; can be used multiple times (passed to "l3build" if patched l3build is detected)')  # noqa: E501
+                    help='print more information; given twice enables debug logging and would be passed to "l3build" if patched l3build is detected)')  # noqa: E501
 
 _inherited = parser.add_argument_group('inherited l3build options')
 # commonly used l3build options
@@ -342,22 +378,18 @@ _inherited.add_argument('-H', '--halt-on-error', action='store_true',
 _inherited.add_argument('--show-log-on-error', action='store_true',
                         default=False)
 
-_improved = parser.add_argument_group('improved l3build options')
+_improved = parser.add_argument_group('enhanced l3build options')
 # modified options
 _improved.add_argument('-q', '--quiet',
                        action=argparse.BooleanOptionalAction,
                        default=True,
-                       help='suppress TeX standard output (local patch added support for "save" target)')  # noqa: E501
+                       help='suppress TeX standard output (support for "save" target needs local l3build patch)')  # noqa: E501
 # fmt: on
 
 if __name__ == '__main__':
     try:
-        init_logging()
-
         args = parser.parse_intermixed_args()
-        update_logging(args.verbose)
-        logger.debug('Parsed args: %s', args)
-
+        set_logging_level(args)
         wrap_l3build(args)
     except (argparse.ArgumentError, argparse.ArgumentTypeError) as e:
         logger.error(str(e))
