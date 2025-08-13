f80c8ac17c42bdbe1b2861bbf6573392417dd915
Yukai Chou <muzimuzhi@gmail.com>
2025-08-13 17:08:24 +0800

    update a comment

--- l3build-wrapper/src/l3build_wrapper/l3build_wrapper.py
+++ l3build-wrapper/src/l3build_wrapper/l3build_wrapper.py
@@ -8,7 +8,7 @@
 # Python 3.12 is needed by `if type not in Target`: check if a `EnumStr`
 # class contains some value.
 # https://docs.python.org/3/library/enum.html#enum.EnumType.__contains__
-# Required python version is also recorded in `ruff.toml`.
+# Required python version is also recorded in `pyproject.toml`.
 
 """Check and save selective l3build tests made easier."""
 
