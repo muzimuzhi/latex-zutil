0f1f17958985bf210308d4c1f8828d374b76e59c
Yukai Chou <muzimuzhi@gmail.com>
2026-07-28 07:06:38 +0800

    update comment on l3build envvars

--- build.lua
+++ build.lua
@@ -6,10 +6,11 @@ module  = ""
 modules = { "zutil", "tabularray" }
 
 --[[
-Unfortunately, `diffext` and `diffexe` MUST be set as envvars, not `l3build`
-variables. See feature request https://github.com/latex3/l3build/issues/400.
+`diffext` and `diffexe` MUST be set as envvars, not `l3build` variables.
+See feature request https://github.com/latex3/l3build/issues/400.
 
-For now these two envvars are set in `mise.toml`.
+For now these two envvars are set in `mise.toml` and taken into account if
+`mise` is activated or when running `mise` tasks.
 
 -- set OS-independent diff file extension and program
 diffext = ".diff"
