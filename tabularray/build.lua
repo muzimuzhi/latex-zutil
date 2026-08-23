ec64b21f351860fbffbca2afe2b7573bd49d447f
Yukai Chou <muzimuzhi@gmail.com>
2026-08-24 04:35:01 +0800

    test(tblr): disable both testsuites

--- tabularray/build.lua
+++ tabularray/build.lua
@@ -13,6 +13,8 @@ checkruns       = 2
 checksuppfiles  = { "zutil-regression-test.cfg" }
 
 
-checkconfigs    = { "build", "config-old" }
+-- TODO: uncomment this when `tabularray` resumes updates. Last release was
+--       2025C (2025-11-27).
+-- checkconfigs    = { "build", "config-old" }
 
 dofile(maindir .. "/support/build-normalize-log.lua")
